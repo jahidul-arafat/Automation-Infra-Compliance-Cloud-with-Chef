7c1fdae18e30dd93c6439f95331c187aeb0f310a
jahidul-arafat <jahidapon@gmail.com>
2021-09-09 12:42:01 +0600

    updated

--- 3_Learn the Chef Infra Language/lab07_motd/hello.rb
+++
@@ -1,3 +0,0 @@
-file '/tmp/motd' do
-  content 'Hello World!!'
-end
--- 4_Test Expectations with Chef InSpec/learn-inspec/output/nmap/groups/sg-07157ff490be6be3f.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# -sT look for connect success
-# -sV, --version-all look for versions of services
-# -T2 relatively slow
-nmap -oX /opt/sgCheckup/results/sg-07157ff490be6be3f.xml -Pn -sT -sV --version-light -T2 -p T:7575 52.55.22.138 52.70.229.165 18.211.142.157
